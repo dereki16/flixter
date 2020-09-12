89a498e37b53caaeba54350a37937b3b2965bab9
dereki16 <derekiniguez@gmail.com>
2020-09-12 04:51:37 -0700

    part 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Derek Iniguez on 9/12/20.
+//  Copyright © 2020 Derek Iniguez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
